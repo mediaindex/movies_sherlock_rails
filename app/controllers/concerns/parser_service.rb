36a706823aa2f20edd6fb21c7785fef2d331984c
Kirill <kirillbrown@gmail.com>
2016-02-25 00:28:53 +0600

    Add parser_service & show_film to controller concerns

---
+++ app/controllers/concerns/parser_service.rb
@@ -0,0 +1,32 @@
+class ParserService
+  attr_reader :result
+
+  def initialize
+    @result = []
+  end
+
+  def find_movie(movie_title)
+    @result = prepare_data(movie_title)
+  end
+
+  def prepare_to_model
+    info =
+        { film_type: result['Type'],
+          imbd_rating: result['imdbRating'],
+          imbd_votes: result['imbdVotes'],
+          imbd_id: result['imbdID']
+        }
+    %i(title year rated released runtime genre director writer actors plot
+   language country awards poster metascore response error).each do |key|
+      info[key] = result[key.to_s.capitalize]
+    end
+    info
+  end
+
+  private
+
+  def prepare_data(movie_title)
+    api_result = RestClient.get 'http://www.omdbapi.com/?t=' + movie_title + '&y=&plot=full&r=JSON'
+    JSON.parse(api_result)
+  end
+end
---
+++ app/controllers/concerns/show_film.rb
@@ -0,0 +1,8 @@
+class ShowFilm
+  def initialize(args)
+    # { title => "Title" ...}
+    args.each do |method_name, value|
+      self.class.send(:define_method, method_name.to_s) { value }
+    end
+  end
+end
