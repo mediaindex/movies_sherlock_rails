1bcef77c22cc9d844b802df361f05ce713bf061a
Kirill <kirillbrown@gmail.com>
2016-02-25 00:32:58 +0600

    Add create action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,29 @@
 class MoviesController < ApplicationController
-  def index
-    @movie = Movie.new
+  def index; end
+
+  def new; end
+
+  def create
+    @movie_title = params[:movie_title]
+    if @movie_title.empty?
+      flash.now[:error] = 'Oops, nothing to search!'
+      render :index
+    elsif @movie_title.match(/[\p{L}&&[^a-zA-Z]]/)
+      flash.now[:error] = 'Please, use only ASCII characters.'
+      render :index
+    else
+      parser = ParserService.new
+      parser.find_movie(@movie_title)
+      parser_result = parser.result
+
+      if parser_result['Title'].nil?
+        flash.now[:error] = "Sorry, but there is no results for \"#{@movie_title}\"."
+        render :index
+
+      else
+        @movie = ShowFilm.new(parser.prepare_to_model)
+        render :show
+      end
+    end
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ActiveRecord::Base
   validates :title, presence: true
+  scope :user_search, ->(user) { where(user_id: user.id) }
 end
