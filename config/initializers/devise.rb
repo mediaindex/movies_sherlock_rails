ae5b8cf026b30efbb67e050cf3233186292f4610
Kirill <kirillbrown@gmail.com>
2016-03-06 14:19:36 +0600

    Add figaro & rails_12factor gems, set app variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,10 +239,10 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  API_KEYS = YAML::load_file("#{Rails.root}/config/api_keys.yml")[Rails.env]
-  config.omniauth :twitter, API_KEYS['twitter']['api_key'], API_KEYS['twitter']['api_secret']
-  config.omniauth :google_oauth2, API_KEYS['google_oauth2']['api_key'], API_KEYS['google_oauth2']['api_secret']
-  config.omniauth :github, API_KEYS['github']['api_key'], API_KEYS['github']['api_secret']
+
+  config.omniauth :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  config.omniauth :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
+  config.omniauth :github,  ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
