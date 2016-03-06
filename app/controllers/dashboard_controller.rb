318029fc3d83da9843f1c9be292f98af96dcc116
Kirill <kirillbrown@gmail.com>
2016-03-06 17:08:59 +0600

    Fix some markup on all pages

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,9 +4,9 @@ class DashboardController < ApplicationController
 
   def index
     authorize :dashboard, :index?
-    @all_users = User.count
-    @all_movies = Movie.count
-    @unique_movies = Movie.uniq.count(:title)
+    @count_all_users = User.count
+    @count_all_movies = Movie.count
+    @count_unique_movies = Movie.uniq.count(:title)
     @popular_movies = Movie.group(:title).count.sort_by { |_key, values| - values}.first(5)
   end
 end
