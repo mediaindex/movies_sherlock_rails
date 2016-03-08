176993fbfcf11d381dde392d6f2608ede9b49c49
Kirill <kirillbrown@gmail.com>
2016-03-08 20:16:03 +0600

    Add device mailer in production, delete unnecessary files

--- app/mailers/application_mailer.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default_url_options[:host] = 'localhost:3000'
-  default from: 'movies_sherlock@example.com'
-end
--- app/mailers/notifier.rb
+++
@@ -1,7 +0,0 @@
-class Notifier < ApplicationMailer
-  def password_reset(user)
-    @user = user
-    mail(to: "#{user.first_name} #{user.last_name} <#{user.email}>",
-         subject: 'Reset your password')
-  end
-end
