fedbb8a1682453b8533eb0ee37b7fd0633741f5a
Kirill <kirillbrown@gmail.com>
2016-03-19 12:28:25 +0600

    Edit routes & links

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,23 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'callbacks' }
 
-  resources :users, only: [:show] do
-    member do
-      get 'my_movies', as: :my_movies
-    end
-  end
+  resources :users, only: [:show]
+
   resources :movies, only: [:index, :create, :show] do
     member do
       post 'vote_for'
       post 'vote_against'
     end
   end
+
   resources :dashboard, only: [:index]
 
   scope '(:locale)', :locale => /en|ru/ do
     root 'movies#index'
   end
+
+  get '/:id' => 'users#show', as: :users
+  get '/:id/my_movies' => 'users#my_movies', as: :my_movies
 
   get '*path' => redirect('/')
 end
