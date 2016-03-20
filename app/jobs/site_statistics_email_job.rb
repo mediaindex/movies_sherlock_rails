bee1b7f12624fb5f63dc5ecb98e83e7f745b89cd
Kirill <kirillbrown@gmail.com>
2016-03-20 23:05:42 +0600

    Add new statistics fields to mailer throught job

--- app/jobs/site_statistics_email_job.rb
+++ app/jobs/site_statistics_email_job.rb
@@ -6,6 +6,8 @@ class SiteStatisticsEmailJob
     count_all_users = dashboard.count_all_users
     count_all_movies = dashboard.count_all_movies
     popular_movies = dashboard.popular_movies
-    UserMailer.site_statistics_email(count_all_users, count_all_movies, popular_movies).deliver_now
+    low_voted_movies = dashboard.low_voted_movies
+    top_voted_movies = dashboard.top_voted_movies
+    UserMailer.site_statistics_email(count_all_users, count_all_movies, popular_movies, low_voted_movies, top_voted_movies).deliver_now
   end
 end
